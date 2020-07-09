8fc86bd2df5b2fa771cea1eb914fbe7a031245e2
Egg Long <eddres229@gmail.com>
2020-07-09 16:08:29 -0400

    Initial Commit

---
+++ sfml/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sfml
+//
+//  Created by Eli Dresnin on 7/9/20.
+//  Copyright © 2020 Eli Dresnin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
